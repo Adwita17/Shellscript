dd6737023fc42d2bdc08a8c68ef9b47c5654489b
Adwita17 <shalinid3011@gmail.com>
2023-05-15 14:37:21 +0530

    Makefile

---
+++ Makefile/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "header.h"
+
+void main()
+{
+	printf("\n Main Function\n");
+	printf("\n Hello function");
+	mul(5,5);
+	printf("\n Add function");
+	add(10,5);
+	printf("\n back to main");
+}
---
+++ Makefile/mul.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "header.h"
+
+void mul(int a, int b)
+{
+	int c;
+	c=a*b;
+	printf("\nmul=%d",c);
+}
